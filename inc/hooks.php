49d0491e85cf7885b4838b86dc6c1afee16ea9c6
pauloiankoski <contato@paulor.com.br>
2015-06-12 10:20:10 -0300

    Modify body classes

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,4 +1,10 @@
 <?php
-/**
- * Vulcano custom hooks.
- */
+function add_slug_body_class( $classes ) {
+	global $post;
+
+	if ( isset( $post ) )
+		$classes[] = $post->post_type . '-' . $post->post_name;
+
+	return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
